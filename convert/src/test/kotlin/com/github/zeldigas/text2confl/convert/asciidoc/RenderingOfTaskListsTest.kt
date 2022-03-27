9fed2726d18e579c09c04222e9d7fd77feaafcd7
Dmitry Pavlov <zeldigas@gmail.com>
2022-03-28 00:09:19 +0300

    feat(asciidoctor): support for checklists including proper rendering of partial (as regular unordered list)

--- convert/src/test/kotlin/com/github/zeldigas/text2confl/convert/asciidoc/RenderingOfTaskListsTest.kt
+++ convert/src/test/kotlin/com/github/zeldigas/text2confl/convert/asciidoc/RenderingOfTaskListsTest.kt
@@ -1,23 +1,20 @@
 package com.github.zeldigas.text2confl.convert.asciidoc
 
 import assertk.assertThat
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
-import org.junit.jupiter.params.provider.ValueSource
+import org.junit.jupiter.api.Test
 
 internal class RenderingOfTaskListsTest : RenderingTestBase() {
 
-    @ValueSource(strings = ["*", "1."])
-    @ParameterizedTest
-    internal fun `Well formatted task list is rendered as confluence task list`(marker: String) {
+    @Test
+    internal fun `Well formatted task list is rendered as confluence task list`() {
         val result = toHtml(
             """
-            $marker [ ] open task
-            $marker [x] closed task
-            $marker [ ] open task _with formatting_
-            $marker [x] complex task with multiline
-            
-                block
+            * [ ] open task
+            * [x] closed task
+            * [ ] open task _with formatting_
+            * [x] complex task with multiline
+            +
+            block
         """.trimIndent()
         )
 
@@ -27,44 +24,33 @@ internal class RenderingOfTaskListsTest : RenderingTestBase() {
             <ac:task><ac:task-status>incomplete</ac:task-status><ac:task-body>open task</ac:task-body></ac:task>
             <ac:task><ac:task-status>complete</ac:task-status><ac:task-body>closed task</ac:task-body></ac:task>
             <ac:task><ac:task-status>incomplete</ac:task-status><ac:task-body>open task <em>with formatting</em></ac:task-body></ac:task>
-            <ac:task><ac:task-status>complete</ac:task-status><ac:task-body>complex task with multiline
-            <p>block</p>
-            </ac:task-body></ac:task>
-            </ac:task-list>
+            <ac:task><ac:task-status>complete</ac:task-status><ac:task-body>complex task with multiline<p>block</p></ac:task-body></ac:task></ac:task-list>
         """.trimIndent(),
         )
     }
 
-    @CsvSource(value = [
-        "*,ul",
-        "1.,ol"
-    ])
-    @ParameterizedTest
-    internal fun `Partial task list is rendered as simple unordered list`(marker:String, htmlTag:String) {
+    @Test
+    internal fun `Partial task list is rendered as simple unordered list`() {
         val result = toHtml(
             """
-            $marker [ ] open task
-            $marker [x] closed task
-            $marker [X] closed task 1
-            $marker [ ] open task _with formatting_
-            $marker simple item
-            
-                block
+            * [ ] open task
+            * [x] closed task
+            * [X] closed task 1
+            * [ ] open task _with formatting_
+            * simple item
+            +
+            block
         """.trimIndent()
         )
 
         assertThat(result).isEqualToConfluenceFormat(
             """
-            <$htmlTag>
-              <li>[ ] open task</li>
-              <li>[x] closed task</li>
-              <li>[X] closed task 1</li>
-              <li>[ ] open task <em>with formatting</em></li>
-              <li>
-                <p>simple item</p>
-                <p>block</p>
-              </li>
-            </$htmlTag>
+            <ul>
+            <li>[ ] open task</li>
+            <li>[X] closed task</li>
+            <li>[X] closed task 1</li>
+            <li>[ ] open task <em>with formatting</em></li>
+            <li>simple item<p>block</p></li></ul>
         """.trimIndent(),
         )
     }
